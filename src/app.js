d8fc84ce536c0cdcb14cd7149d3eca0d8c74559c
juanrar <juanchilorenzo@gmail.com>
2026-08-03 20:31:24 -0300

    feat: implement Express server with Handlebars and product route

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import { engine } from 'express-handlebars';
+import viewsRouter from './routes/views.router.js'
+
+const app =  express();
+
+// Middlewares básicos
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Configurar carpeta public
+app.use(express.static('src/public'));
+
+// Configuraciones de handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
+app.use('/', viewsRouter);
+
+const serverExp = app.listen(8080, () => {
+    console.log("Servidor ON en puerto 8080");
+});
+
+const socketServer = new Server(serverExp);
+socketServer.on('connection', (socket) => {
+    console.log("Se conectó alguien por websocket: ", socket.id);
+});
---
+++ src/routes/views.router.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/products', async (req, res) => {
+    
+    const testProducts = [
+        { 
+            _id: "123", 
+            title: "Grand Theft Auto: San Andreas", 
+            price: 19.99, 
+            stock: 15, 
+            thumbnails: ["https://images.launchbox-app.com/56b5479a-bba5-43d6-b458-0fcd00bf10ba.png"] 
+        },
+        { 
+            _id: "456", 
+            title: "Resident Evil 4", 
+            price: 17.99, 
+            stock: 10, 
+            thumbnails: ["https://images.launchbox-app.com/r2_acc0564e-f465-405b-bf84-0ee721b1714d.png"] 
+        }
+    ];
+
+    res.render('products', { 
+        payload: testProducts,  
+        code: '200 papa',
+    });
+});
+
+export default router;
