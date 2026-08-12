4282a8145f85a0f495b0f27a943559f5fcfc5871
juanrar <juanchilorenzo@gmail.com>
2026-08-12 19:11:39 -0300

    feat: remove unused views, styles, and socket.io integration from the project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { Server } from 'socket.io';
-import { engine } from 'express-handlebars';
-import viewsRouter from './routes/views.router.js'
 import productsRouter from './routes/product.router.js'
 import cartRouter from './routes/cart.router.js'
 
@@ -13,34 +10,17 @@ const app =  express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Configurar carpeta public
-app.use(express.static('src/public'));
-
-// Configuraciones de handlebars
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './src/views');
-
 // MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
     .then(() => console.log('MongoDB conectado'))
     .catch(err => console.log('Error al conectar a Mongo:', err));
 
 
-// Rutas Vistas
-app.use('/', viewsRouter);
-
 // Rutas Producto
  app.use('/api/products', productsRouter)
- app.use('/api/cart', cartRouter)
+ app.use('/api/carts', cartRouter)
 
 
 const serverExp = app.listen(8080, () => {
     console.log("Servidor ON en puerto 8080");
-});
-
-const socketServer = new Server(serverExp);
-socketServer.on('connection', (socket) => {
-    console.log('Se conectó alguien por websocket:', socket.id);
-    socket.emit('saludo', 'Te saludo desde muy lejos');
 });
--- src/public/index.js
+++
@@ -1,6 +0,0 @@
-const socket = io();
-
-function agregarAlCarrito(pid) {
-    console.log("Pronto agregaremos el juego con ID: " + pid);
-    Swal.fire('¡En construcción!', 'Pronto se agregará al carrito', 'info');
-}
